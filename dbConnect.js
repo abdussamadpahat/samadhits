8ce428bf2d41709f234e7ef8614ba82df55f65bf
abdussamadpahat <150114091+abdussamadpahat@users.noreply.github.com>
2024-01-26 15:00:36 +0530

    Update dbConnect.js changed

--- dbConnect.js
+++ dbConnect.js
@@ -1,11 +1,13 @@
 const mongoose = require("mongoose")
 
+const db = "mongodb+srv://samadhits:samadhits12345@cluster0.0052s6x.mongodb.net/samadhitsDB"
+
 async function getConnect(){
     try {
         // await mongoose.connect(process.env.DB_KEY)
         // await mongoose.connect("mongodb://127.0.0.1:27017/videoPlayerDB")
 
-        await mongoose.connect("mongodb+srv://abdussamadkhan:Samad12345@cluster0.84rxiv5.mongodb.net/samadhitsDB")
+        await mongoose.connect(db)
         console.log("Database is connected!!!");
     } catch (error) {
         console.log(error);
